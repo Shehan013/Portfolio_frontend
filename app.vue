a185ccff665da5bcb1699db4bc00692ef4171210
shehan013 <shehanmaleesha13@gmail.com>
2024-05-16 21:57:01 +0530

    project_info

--- app.vue
+++ app.vue
@@ -3,4 +3,34 @@
     <h1>Shehan Lokuwella</h1>
     <p>IT Undergraduate of University of Moratuwa</p>
   </div>
+  <div>
+    <h2>{{ name }}</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
+
+// add script and style tags
+
+<script setup>
+
+const name = 'Shehan Lokuwella';
+const projects = [
+   {
+    name : 'Project 1',
+    description : 'This is project 1'
+   },
+
+   {
+    name : 'Project 2',
+    description : 'This is project 2'
+   },
+  ];
+</script>
+
+<style></style>
+
